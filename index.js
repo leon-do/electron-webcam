662acbfa08a90dfe27665643dadeb5e44613999e
leon-do <leondo123@gmail.com>
2023-07-02 02:03:09 -0400

    frameless

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { app, BrowserWindow } = require("electron");
-require("electron-reload")(__dirname);
 
 function createWindow() {
   // Create a new browser window
   const mainWindow = new BrowserWindow({
     width: 500,
     height: 500,
+    frame: false, // Remove window frame
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true, // Added to enable remote module access
@@ -14,10 +14,6 @@ function createWindow() {
 
   // Load the index.html file
   mainWindow.loadFile("index.html");
-
-  mainWindow.once("ready-to-show", () => {
-    mainWindow.show();
-  });
 }
 
 // When Electron has finished initialization and is ready to create browser windows
