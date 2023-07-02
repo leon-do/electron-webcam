a1f22e1b3e2f04fb269a7be4fbed543818655af2
leon-do <leondo123@gmail.com>
2023-07-02 01:34:08 -0400

    no flicker

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ require("electron-reload")(__dirname);
 function createWindow() {
   // Create a new browser window
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 800,
+    width: 500,
+    height: 500,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true, // Added to enable remote module access
@@ -15,26 +15,10 @@ function createWindow() {
   // Load the index.html file
   mainWindow.loadFile("index.html");
 
-  // Handle the resize event to enforce the square shape
-  mainWindow.on("resize", () => {
-    const [width, height] = mainWindow.getSize();
-    const size = Math.max(width, height);
-    mainWindow.setSize(size, size);
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
   });
 }
 
 // When Electron has finished initialization and is ready to create browser windows
 app.on("ready", createWindow);
-
-// Quit the app when all windows are closed
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
